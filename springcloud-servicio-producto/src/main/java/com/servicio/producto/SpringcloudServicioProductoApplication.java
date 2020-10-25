bd9393a2cc1054404e38276ba1590b3d7038a68d
walterpalomino <jimmypalomino93@gmail.com>
2020-10-24 23:24:26 -0300

    iniciando proyecto servicio producto cloud

---
+++ springcloud-servicio-producto/src/main/java/com/servicio/producto/SpringcloudServicioProductoApplication.java
@@ -0,0 +1,13 @@
+package com.servicio.producto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudServicioProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudServicioProductoApplication.class, args);
+	}
+
+}
---
+++ springcloud-servicio-producto/src/test/java/com/servicio/producto/SpringcloudServicioProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudServicioProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
